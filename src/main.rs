e334ed62846b1dc432490e35951e82bf572491c9
Ryo-sunken <bellman.toratora@gmail.com>
2024-09-23 23:03:15 +0900

    画像出力

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+mod vec3;
+
+const IMAGE_WIDTH: u32 = 256;
+const IMAGE_HEIGHT: u32 = 256;
 fn main() {
-    println!("Hello, world!");
+    println!("P3");
+    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
+    println!("255");
+
+    for i in 0..IMAGE_HEIGHT {
+        for j in 0..IMAGE_WIDTH {
+            let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let b = 0.25;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
 }
---
+++ src/vec3.rs
@@ -0,0 +1,7 @@
+#[derive(Debug, Clone, Copy)]
+struct Vector3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
